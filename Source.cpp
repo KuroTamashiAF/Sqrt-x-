f4087b5a1c31c98a0f584ad287eb5e6f9abd98dc
Fedorov <chugun777akk@mail.ru>
2023-04-05 22:06:45 +0700

    create source

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
-
+// извлечение квадратного корня 
 class Solution {
 public:
-    int mySqrt(int x) {
+    int mySqrt(int x)
+    {
+        if (x == 0)
+            return x;
 
-
-
-
+        int l = 1;
+        int r = x;
+        long result;
+        while (l <= r)
+        {
+            long mid = l + (r - l) / 2;
+            if (mid * mid == x)
+            {
+                result = mid;
+                return (int)result;
+            }
+            else if (mid * mid < x)
+            {
+                l = mid + 1;
+            }
+            else
+                r = mid - 1;
+        }
+        
     }
 };
 
 int main()
 {
-    int num = 4;
+    int num = 63;
     Solution x; 
     std::cout << x.mySqrt(num) << std::endl;
     return 0;
