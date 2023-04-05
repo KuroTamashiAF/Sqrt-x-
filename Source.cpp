9fa74a12fdf5c23ffb3ca57ca904eb37432f3cf4
Fedorov <chugun777akk@mail.ru>
2023-04-05 19:59:48 +0700

    add source

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+class Solution {
+public:
+    int mySqrt(int x) {
+
+
+
+
+    }
+};
+
+int main()
+{
+    int num = 4;
+    Solution x; 
+    std::cout << x.mySqrt(num) << std::endl;
+    return 0;
+}
+
+
+
+
